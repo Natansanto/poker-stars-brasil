267ed4eb4ce7b848592c8ffe1ab3a16078fce4a4
Natansanto <ads.natansantos@gmail.com>
2019-04-20 14:32:53 -0300

    persistence.xml configurado

--- poker-stars-brasil/src/main/java/br/com/utils/ConnectionFactory.java
+++ poker-stars-brasil/src/main/java/br/com/utils/ConnectionFactory.java
@@ -1,12 +1,15 @@
 package br.com.utils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class ConnectionFactory {
 
-	
-	
-	
-	
-	
-	
-	
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("poker-brasil");
+
+	public EntityManager getConnction() {
+		return emf.createEntityManager();
+	}
+
 }
